ee89ee416e4839eb28cdfed3f2eb22980aa05804
cruizji <57686387+cruizji@users.noreply.github.com>
2020-10-04 18:19:37 +0200

    Update test_container.sh

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -6,12 +6,16 @@ while true
 do
   ((contador=contador+1))
   STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8000)
-  if [ $STATUS -eq 200 ] || [ $contador -eq 3 ]; then
+  if [ $STATUS -eq 200 ]; then
     echo "Got 200! All done!"
-    break
+    exit 0
+  elif [ $contador -eq 3 ]; then
+    echo " Sales por pesado"
+    exit 1
   else
     echo "Got $STATUS :( Not done yet..."
     sleep 1
   fi
 
 done
+if
