1459ced890f28f05f46fa216f673036402d4cf88
cruizji <57686387+cruizji@users.noreply.github.com>
2020-10-04 13:54:33 +0200

    Update test_container.ps1

--- scripts/test_container.ps1
+++ scripts/test_container.ps1
@@ -1,18 +1,17 @@
-$count = 0
-do {
-    $count++
-    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
-    
-    $testStart = Invoke-WebRequest -Uri http://localhost:8000
-    
-    if ($testStart.statuscode -eq '200') {
-        $started = $true
-    } else {
-        Start-Sleep -Seconds 1
-    }
-    
-} until ($started -or ($count -eq 3))
 
-if (!$started) {
-    exit 1
-}
+#!/bin/bash
+contador=0
+
+while true
+do
+  ((contador=contador+1))
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8000)
+  if [ $STATUS -eq 200 ] || [ $contador -eq 3 ]; then
+    echo "Got 200! All done!"
+    break
+  else
+    echo "Got $STATUS :( Not done yet..."
+    sleep 1
+  fi
+
+done
