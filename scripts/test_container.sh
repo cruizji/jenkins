09eae3f8900c06c646ec45f2ceef43fb8ecd9782
cruizji <57686387+cruizji@users.noreply.github.com>
2020-10-04 18:22:42 +0200

    Update test_container.sh

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -5,7 +5,7 @@ contador=0
 while true
 do
   ((contador=contador+1))
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8001)
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8000)
   if [ $STATUS -eq 200 ]; then
     echo "Got 200! All done!"
     exit 0
